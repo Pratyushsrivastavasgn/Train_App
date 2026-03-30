1c4abca2846dda106ae7639bc2fefe32be604edf
Pratyushsrivastavasgn <pratyushsrivastava1sgn@gmail.com>
2026-03-30 15:09:45 +0530

    UC7

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,29 +1,39 @@
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class TrainConsistManagementApp {
 
+    static class Bogie {
+        String name;
+        int capacity;
+
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("=====================================");
-        System.out.println("   UC5 - Preserve Insertion Order of Bogies");
+        System.out.println("   UC7 - Sort Bogies by Capacity");
         System.out.println("=====================================\n");
 
-        // Create LinkedHashSet
-        Set<String> formation = new LinkedHashSet<>();
+        List<Bogie> bogies = new ArrayList<>();
 
-        formation.add("Engine");
-        formation.add("Sleeper");
-        formation.add("Cargo");
-        formation.add("Guard");
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 40));
+        System.out.println("Before Sorting:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
 
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
 
-        formation.add("Sleeper");
+        System.out.println("\nAfter Sorting (by capacity):");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
 
-        // Display final formation
-        System.out.println("Final Train Formation:");
-        System.out.println(formation);
-
-        System.out.println("\nUC5 operations completed successfully...");
+        System.out.println("\nUC7 operations completed successfully...");
     }
 }
